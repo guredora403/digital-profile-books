5678e2ef49e87852e4ca254e9d7d7b474addf331
guredora <contact@guredora.com>
2024-11-22 02:15:58 +0900

    add login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { createClient } from "~/utils/supabase/client"
+import { Auth } from "@supabase/auth-ui-react"
+import { useEffect, useState } from "react"
+
+export default function Page() {
+    const [supabase, setSupabase] = useState<ReturnType<typeof createClient> | null>(null)
+    useEffect(() => {
+        setSupabase(createClient())
+        return () => {}
+    }, [])
+    return <div>
+        <h1>Supabase Auth</h1>
+        {supabase && <Auth supabaseClient={supabase} providers={["github", "discord"]} onlyThirdPartyProviders={true} redirectTo="http://localhost:3000/auth/callback"/>}
+    </div>
+}
