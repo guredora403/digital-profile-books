a51d2c39f1c0b207c162c0703e89a567443ddf73
guredora <contact@guredora.com>
2025-01-19 01:13:15 +0900

    update login page to conditionally allow only third-party providers in development mode

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 import { createClient } from "~/utils/supabase/client"
 import { Auth } from "@supabase/auth-ui-react"
 import { useEffect, useState } from "react"
+import { env } from "~/env"
 
 export default function Page() {
     const [supabase, setSupabase] = useState<ReturnType<typeof createClient> | null>(null)
@@ -11,6 +12,6 @@ export default function Page() {
     }, [])
     return <div>
         <h1>Supabase Auth</h1>
-        {supabase && <Auth supabaseClient={supabase} providers={["github", "discord"]} onlyThirdPartyProviders={false} redirectTo="http://localhost:3000/auth/callback"/>}
+        {supabase && <Auth supabaseClient={supabase} providers={["github", "discord"]} onlyThirdPartyProviders={(env.NODE_ENV === "development")} redirectTo="http://localhost:3000/auth/callback"/>}
     </div>
 }
