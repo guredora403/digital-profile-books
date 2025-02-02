e693a54bf32c6f8b2d420d263aea590093763e30
guredora <contact@guredora.com>
2025-02-02 09:29:41 +0900

    名刺帳登録ボタンの挙動がおかしかったのを修正

--- src/app/_components/ShowOtherIdentityInfo.tsx
+++ src/app/_components/ShowOtherIdentityInfo.tsx
@@ -4,28 +4,32 @@ import { type CardInfo } from "~/models/card";
 import { api } from "~/trpc/react";
 import { ProfileView } from "./ProfileView";
 import JSONPretty from "react-json-pretty";
+import { useRouter } from "next/navigation";
 
 export const ShowOtherCardInfo = ({ cardInfo: cardInfo }: { cardInfo: CardInfo }) => {
     const createMutation = api.me.cardbooks.create.useMutation();
     const removeMutation = api.me.cardbooks.delete.useMutation();
     const util = api.useUtils();    
+    const router = useRouter();
     return <div>
         {cardInfo.addedToCardBook ? 
         <Button onPress={(_) => {
             removeMutation.mutate({targetId: cardInfo.id}, {
                 onSuccess: () => {
                     void util.invalidate()
+                    router.refresh();
                 },
                 onError: (error) => {
                     //todo: error again
                     console.error(error)
                 }
             });
-        }}>登録解除</Button>: 
+        }} isPending={removeMutation.isPending}>登録解除</Button>: 
         <Button onPress={(_) => {
             createMutation.mutate({targetId: cardInfo.id}, {
                 onSuccess: () => {
                     void util.invalidate()
+                    router.refresh();
                 },
                 onError: (error) => {
                     //todo: show error
